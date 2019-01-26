01ee1bad6b87b4bccda1e2f8525a7c7648ae589c
yuki15966 <yuki15966@gmail.com>
2019-01-26 14:44:04 +0100

    Added the web storage

--- frontend/js/routes/startRoute.js
+++ frontend/js/routes/startRoute.js
@@ -2,4 +2,18 @@ function startRoute() {
 
   var vid = document.getElementById("StartSound");
   vid.play();
+
+var input = document.getElementById("username");
+var storedValue = localStorage.getItem("storage");
+if(storedValue != null)
+{
+  input.setAttribute("placeholder", storedValue);
+}
+
+       document.getElementById("StartButton").onclick = function () {
+       localStorage.setItem("storage", input.value);
+
+
+            location.href = "#play";
+        };
 }
